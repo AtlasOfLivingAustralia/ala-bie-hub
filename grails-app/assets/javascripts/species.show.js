9550e657530a0752dcc0fc77fb4a4da818823e6d
Sushant <105408469+sughics@users.noreply.github.com>
2023-02-01 15:53:44 +1100

    issue #7 removing redundant underscore replacements and capitalisations for triat names and values

--- grails-app/assets/javascripts/species.show.js
+++ grails-app/assets/javascripts/species.show.js
@@ -225,8 +225,8 @@ function loadAusTraits() {
         if (data.numeric_traits && data.categorical_traits) {
             $.each(data.categorical_traits, function (idx, traitValue) {
                 var tableRow = "<tr><td>";
-                tableRow += capitalise(replaceUnderscore(traitValue.trait_name)) + "</td><td>"
-                tableRow += replaceUnderscore(traitValue.trait_values) + "</td><td class='centered-cell'>"
+                tableRow += traitValue.trait_name + "</td><td>"
+                tableRow += traitValue.trait_values + "</td><td class='centered-cell'>"
                 tableRow += "<a target='_blank' href=" + traitValue.definition + "> <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-box-arrow-up-right\" viewBox=\"0 0 16 16\">\n" +
                     "  <path fill-rule=\"evenodd\" d=\"M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z\"/>\n" +
                     "  <path fill-rule=\"evenodd\" d=\"M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z\"/>\n" +
@@ -238,7 +238,7 @@ function loadAusTraits() {
             $.each(data.numeric_traits, function (idx, traitValue) {
                 console.log(traitValue.min, traitValue.mean, traitValue.max)
                 var tableRow = "<tr><td>";
-                tableRow += capitalise(replaceUnderscore(traitValue.trait_name)) + "</td><td class='centered-cell'>"
+                tableRow += traitValue.trait_name + "</td><td class='centered-cell'>"
                 tableRow += (traitValue.min || " - " ) + "</td><td class='centered-cell'>"
                 tableRow += (traitValue.mean || " - " )+ "</td><td class='centered-cell'>"
                 tableRow += (traitValue.max  || " - " ) + "</td><td class='centered-cell'>"
